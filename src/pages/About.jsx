aaaeacf50ea87670aa22fc4631fc17225ff83925
jluo <jluo8@wgu.edu>
2024-08-13 11:47:57 -0700

    uppdate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/AboutPage";
 
 const About = () => {
-  return (
-    <Wrapper>
-      <div>
-        <div>
-          <h3>about</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-            temporibus beatae nostrum necessitatibus sint officiis nobis
-            molestiae maxime exercitationem? Porro quaerat iste nobis? Cum earum
-            dolores at dignissimos exercitationem vitae.
-          </p>
-        </div>
-      </div>
-    </Wrapper>
-  );
+ return (
+  <Wrapper>
+   <div>
+    <div>
+     <h3>PurpleCat Bar</h3>
+     <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+      temporibus beatae nostrum necessitatibus sint officiis nobis molestiae
+      maxime exercitationem? Porro quaerat iste nobis? Cum earum dolores at
+      dignissimos exercitationem vitae.
+     </p>
+    </div>
+   </div>
+  </Wrapper>
+ );
 };
 
 export default About;
